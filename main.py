5b7c2cf8cc054191e97ae166952d4d82125668b7
Rafael Leao <rslgp@cin.ufpe.br>
2024-11-21 13:48:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+#!pip install transformers diffusers torch
+
+from diffusers import StableDiffusionPipeline
+import torch
+from PIL import Image
+import os
+
+# Load model from Hugging Face
+model_name = "liamhvn/disney-pixar-cartoon-b"
+pipe = StableDiffusionPipeline.from_pretrained(model_name, torch_dtype=torch.float16)
+
+# PROMPT VARIABLES
+prompt = "Generate this person in a Pixar cartoon style; keep hair color in brown spectrum; male gender"
+image_path="/image.jpg"
+input_image = Image.open(image_path)
+
+#ultimate combination
+# Define different numbers of inference steps
+# guidance_scales = [7.5, 10.0, 12.5, 15.0]
+guidance_scales = [7.5]
+steps_list = [75, 100]
+outputs = []
+strengths = [0.25, 0.5]  # Different levels of style intensity
+seeds = [64, 128, 256, 512]
+name = []
+
+# for seed in seeds:
+# generator=torch.manual_seed(seed)
+for scale in guidance_scales:
+  for strength in strengths:
+    for steps in steps_list:
+        pixar_image = pipe(prompt, init_image=input_image, strength=strength, num_inference_steps=steps, guidance_scale=scale).images[0]
+        outputs.append(pixar_image)
+        name.append(f"var_scale{scale}+strg{strength}+steps{steps}")
+        pixar_image.show()
+
+# Save the outputs
+for i, pixar_image in enumerate(outputs):
+    pixar_image.save(f"pixar_output{name[i]}.png")
